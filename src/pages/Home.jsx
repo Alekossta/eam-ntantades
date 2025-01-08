55038d4487bf33eeabbc6cd93aaed716b22013b5
Alekossta <alekosstavrinoudakis@gmail.com>
2025-01-08 12:58:47 +0200

    making home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,30 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default function Home()
 {
-    return <Box sx={{width:"100%", height: "100vh"}}>
+    return <Box sx={{width:"100%", height: "100%"}}>
         <Box
-            component="img"
-            src="https://via.placeholder.com/500" // Replace with your image URL
             sx={{
-            width: '100%',
+                width: '100%', // Adjust the width as needed
+                height: '100vh', // Adjust the height as needed
+                backgroundImage: `url('homeWallpaper.png')`, // Replace with your image URL
+                backgroundSize: 'cover', // Makes sure the image covers the box
+                backgroundPosition: 'center', // Centers the image
+                backgroundRepeat: 'no-repeat', // Prevents the image from repeating
+                padding: "2rem"
             }}
-        />
+        >
+            <Box
+                sx={{
+                    backgroundColor: "rgba(0,0,0,0.5)",
+                    marginTop: "2rem"
+                }}
+            
+            >
+                <Typography variant="h1">
+                    ΝΤΑΝΤΑΔΕΣ
+                </Typography>
+            </Box>
+        </Box>
     </Box>
 }
