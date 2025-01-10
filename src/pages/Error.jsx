ca8493cf438e406057f148ec70fa2d198f021c94
Alekossta <alekosstavrinoudakis@gmail.com>
2025-01-10 11:22:03 +0200

    added funny error page image

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Box } from '@mui/material';
 
 const Error = () => {
     return (
         <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+            <Box
+                component="img"
+                src="error.png"
+                sx={{
+                    width: '30vh', // Adjust width
+                    height: '30vh', // Maintain aspect ratio
+                }}
+            />
             <h1>404 - Page Not Found</h1>
             <p>Sorry, the page you are looking for does not exist.</p>
         </div>
